0b8d9aff95ffbaa92121b586cffcf08191bd2476
Fred Souza <fredericoamsouza@gmail.com>
2021-08-23 22:16:57 -0300

    Clesaning context class

--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
-
 import { AuthState, Http, Routes } from "./utils/http";
 
 import { Auth } from "./auth";
@@ -7,34 +5,21 @@ import { Account } from "./account";
 import { Card } from "./card";
 import { Payment } from "./payment";
 
-interface Adapters {
-  uuid: Function;
-}
-
-interface ApiOptions {
-  uuidAdapter: () => string;
-}
-
 interface NubankApiConstructor {
   certPath?: string;
   accessToken?: string;
   privateUrls?: Routes;
   publicUrls?: Record<string, string>;
-  options?: ApiOptions;
 }
 
 export class Context {
-  private _options: ApiOptions;
   private _http: Http;
   private _auth: Auth;
   private _account: Account;
   private _card: Card;
   private _payment: Payment;
 
-  public constructor(params: NubankApiConstructor = {}) {
-    this._options = params?.options ?? {
-      uuidAdapter: uuidv4
-    };
+  public constructor(params?: NubankApiConstructor) {
     this._http = new Http(params);
     this._auth = new Auth(this);
     this._account = new Account(this);
@@ -44,12 +29,6 @@ export class Context {
 
   public get authState(): AuthState {
     return this._http.authState;
-  }
-
-  public get adapters(): Adapters {
-    return {
-      uuid: this._options.uuidAdapter
-    };
   }
 
   public get http(): Http {
