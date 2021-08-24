72e3b0d22f45c4678bc44e5a7f2388cf2cf3139d
Fred Souza <fredericoamsouza@gmail.com>
2021-08-23 23:40:35 -0300

    Adding example to create pix money request

---
+++ examples/pix-request.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from 'fs';
+
+import { NubankApi, PixKey } from '../src';
+
+const authState = JSON.parse(readFileSync('./auth-state.json').toString('utf8'));
+
+const api = new NubankApi(authState);
+
+(async () => {
+  try {
+    const pixKeys: PixKey[] = await api.account.getPixKeys();
+    console.log(pixKeys);
+    const paymentRequest = await api.payment.createPixPaymentRequest(pixKeys[0], 100);
+    console.log(paymentRequest);
+    process.exit(0);
+  } catch (e) {
+    console.error(e.stack);
+  }
+})();
